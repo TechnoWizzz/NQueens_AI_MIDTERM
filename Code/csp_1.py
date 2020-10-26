8b744d249e9f4bb6ba53fb15d2e327ffbbdb0d4e
TechnoWizzz97 <35902136+TechnoWizzz97@users.noreply.github.com>
2020-10-26 15:15:29 -0400

    Finalized forward checking with proper domain checks for variables

--- Code/csp_1.py
+++ Code/csp_1.py
@@ -1,19 +1,22 @@
 import numpy as np
 import random
-import string
-from copy import deepcopy
 
+#CSP object used to instantiate nqueens setup and includes the various types of searches
+#that you would run on the nqueens initial queen placement
 class csp:
     def __init__(self, col=random.randint(0,7), row=random.randint(0,7)):
         self.board = np.zeros((8,8))
         self.board[row][col] = 1
         self.start= [col,row]
+        self.queen_domains = [[0,1,2,3,4,5,6,7],[0,1,2,3,4,5,6,7],[0,1,2,3,4,5,6,7],[0,1,2,3,4,5,6,7],[0,1,2,3,4,5,6,7],[0,1,2,3,4,5,6,7],[0,1,2,3,4,5,6,7],[0,1,2,3,4,5,6,7]]
         self.placed = 1
-        print(self.start)
+        print(f'{self.start}, formatted as [col,row] starting from 0')
         
+    #Modifies the object's 'board' variable and adds a marker designating a queen has been placed in that location
     def place_queen(self, val, loc):
         self.board[loc[0]][loc[1]] = val
-        
+    
+    #Checks a possible queen placement so that no queen already on the board is contested by it    
     def check_placement(self, col, row):
         #Check column
         if 1 in self.board[row]:
@@ -30,56 +33,69 @@ class csp:
         else:
             return True
             
+    #Initial function call to begin backchecking search and print results when finished
     def backtrack(self):
         if(self.backtack_rec(self.start[0]+1)):
             print(f'Solution has been found as follows:\n{self.board}\n')
         else:
             print(f'A solution could not be found with the given starting queen placement')
     
+    #Recursive function that will iterate through the queen placements until a solution is found or found to be impossiblle with
+    #the given starting queen placement
     def backtack_rec(self, col):
+        #Returns true when all queens have been placed
         if self.placed == 8:
             return True
+        #Compensates for when starting position is not the first column
         if(col == 8):
             col = 0
+        #Iterate through each row of the column that is being filled
         for val in range(8):
+            #Check if col,row placement is possible with the current board
             if self.check_placement(col, val):
+                #Fills place if placement passes check
                 self.placed+=1
                 self.board[val][col] = 1
+                #Continue onto the next column recursively
                 if self.backtack_rec(col+1):
                     return True
+                #Removes placement because if statement above returns false, the placement is a dead end in the search
                 self.board[val][col] = 0
                 self.placed-=1
+        #If all placements are tried and not possible, returns false signaling that the previous column is not the correct placement and backtracks
         return False
-        
+    #Initial function call to begin forward checking search and print results when finished    
     def forward_checking(self):
         if(self.forward_rec(self.start[0]+1)):
             print(f"Solution has been found using forward checking as follow:\n{self.board}")
         else:
             print(f'A solution could not be found with the given starting queen placement:\n{self.start}')
-            
+    
+    #Recursive function that will iterate through the queen placements until a solution is found or found to be impossiblle with
+    #the given starting queen placement
     def forward_rec(self, col):
-    #MODIFY - NEED TO KEEP TRACK OF DOMAINS OF EACH QUEEN POSSIBILITY AT ALL TIME. EX: QUEENS_DOMAINS[[0,1,2,3,4,5,6,7],[0,1,2,3,4,5,6,7],...]
-    #REMOVE FROMQUEENS DOMAIN WHEN MAKING A PLACEMENT
+        #Returns true when all queens have been placed
         if self.placed == 8:
             return True
+        #Compensates for when starting position is not the first column
         if col ==8:
             col = 0
-        possibleRows = []
-        for val in range(0,8):
-            if self.check_placement(col,val):
-                possibleRows.append(val)
-        for row in possibleRows:
-            if self.check_placement(col, row): #REPLACE WITH CHECK_DOMAIN
+        #Forward check possible placements for the current column
+        for val in self.queen_domains[col]:
+            if not self.check_placement(col,val):
+                self.queen_domains[col][val] = -9
+        #Evaluate the next column using possible values from the modified domain of the current column
+        for row in self.queen_domains[col]:
+            if row != -9:
                 self.placed+=1
                 self.board[row][col] = 1
-                #MODIFY DOMAINS HERE AFTER PLACING A QUEEN
-                if self.backtack_rec(col+1):
+                if self.forward_rec(col+1):
                     return True
                 self.board[row][col] = 0
                 self.placed-=1
+        self.queen_domains[col] = [0,1,2,3,4,5,6,7]
         return False
         
-        
-        
+    #def arc_consistency(self)
         
         
--- Code/run.py
+++ Code/run.py
@@ -1,6 +1,12 @@
+#Import csp object and respective functions from the respective py file
 from csp_1 import csp
 
+
+#Create NQueens csp object
 test = csp(0,0)
+#Run backtracking search on nqueens csp
 test.backtrack()
-test = csp()
+#Create NQueens csp object with randomized initial queen placement
+test = csp(0,0)
+#Run forward checking search on randomized NQueens csp object
 test.forward_checking()
